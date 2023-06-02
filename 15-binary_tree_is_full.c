89b86be9a51f522207e1efab6d99ccd8660e3eb8
Pinerealm <osalami09@gmail.com>
2023-06-02 07:43:08 +0300

    Checks if a binary tree is full

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - Checks if a binary tree is full
+ * @tree: Pointer to the root node of the tree
+ *
+ * Return: 1 if the tree is full, 0 otherwise
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int left, right;
+
+	if (tree == NULL)
+		return (0);
+	left = tree->left ? 1 + binary_tree_is_full(tree->left) : 0;
+	right = tree->right ? 1 + binary_tree_is_full(tree->right) : 0;
+
+	if (left == right)
+		return (1);
+	return (0);
+}
