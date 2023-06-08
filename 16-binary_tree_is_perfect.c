6aca56767348262e427fa75b54f0f10ea5e27e00
Pinerealm <osalami09@gmail.com>
2023-06-08 18:10:20 +0300

    Refactor to fulfill conditions for a perfect tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+int _binary_tree_height(const binary_tree_t *tree);
 
 /**
  * binary_tree_is_perfect - Checks if a binary tree is perfect
@@ -8,14 +9,44 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int left, right;
+	int left_height, right_height;
 
-	if (tree == NULL)
+	if (!tree)
 		return (0);
-	left = tree->left ? 1 + binary_tree_is_perfect(tree->left) : 0;
-	right = tree->right ? 1 + binary_tree_is_perfect(tree->right) : 0;
 
-	if (left == right)
+	left_height = _binary_tree_height(tree->left);
+	right_height = _binary_tree_height(tree->right);
+
+	if (left_height != right_height)
+		return (0);
+	if (!tree->left && !tree->right)
 		return (1);
+
+	if (tree->left && tree->right)
+		return (binary_tree_is_perfect(tree->left) &&
+			binary_tree_is_perfect(tree->right));
+
 	return (0);
+}
+
+/**
+ * _binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree
+ *
+ * Return: The height of the tree
+ */
+int _binary_tree_height(const binary_tree_t *tree)
+{
+	int left_height, right_height;
+
+	if (!tree)
+		return (0);
+
+	left_height = _binary_tree_height(tree->left);
+	right_height = _binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+		return (left_height + 1);
+
+	return (right_height + 1);
 }
