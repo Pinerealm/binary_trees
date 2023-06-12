6c93e07aaee783c340add1e50b409f57be537014
Pinerealm <osalami09@gmail.com>
2023-06-12 23:26:09 +0300

    Avoid function calls into null children

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -9,6 +9,11 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		func(tree->n);
+		return;
+	}
 	binary_tree_inorder(tree->left, func);
 	func(tree->n);
 	binary_tree_inorder(tree->right, func);
