6e799d532ce6ecbe9727cedce6e6b76b6d2c8aba
Pinerealm <osalami09@gmail.com>
2023-06-13 15:41:14 +0300

    Use more appropriate variable names

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,5 @@
 #include "binary_trees.h"
-int _binary_tree_height(const binary_tree_t *tree);
+int binary_tree_balance_helper(const binary_tree_t *tree);
 
 /**
  * binary_tree_balance - Measures the balance factor of a binary tree
@@ -9,31 +9,27 @@ int _binary_tree_height(const binary_tree_t *tree);
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height, right_height;
+	int r_factor, l_factor;
 
 	if (tree == NULL)
 		return (0);
 
-	left_height = tree->left ? _binary_tree_height(tree->left) + 1 : 0;
-	right_height = tree->right ? _binary_tree_height(tree->right) + 1 : 0;
-
-	return (left_height - right_height);
+	l_factor = tree->left ? 1 + binary_tree_balance_helper(tree->left) : 0;
+	r_factor = tree->right ? 1 + binary_tree_balance_helper(tree->right) : 0;
+	return (l_factor - r_factor);
 }
 
 /**
- * _binary_tree_height - Measures the height of a binary tree
+ * binary_tree_balance_helper - Helper function for binary_tree_balance
  * @tree: Pointer to the root node of the tree
  *
- * Return: Height of the tree
+ * Return: Balance factor of the tree
  */
-int _binary_tree_height(const binary_tree_t *tree)
+int binary_tree_balance_helper(const binary_tree_t *tree)
 {
-	int height_l, height_r;
+	int left_height, right_height;
 
-	if (tree == NULL)
-		return (0);
-	height_l = tree->left ? 1 + _binary_tree_height(tree->left) : 0;
-	height_r = tree->right ? 1 + _binary_tree_height(tree->right) : 0;
-
-	return (height_l > height_r ? height_l : height_r);
+	left_height = tree->left ? 1 + binary_tree_balance_helper(tree->left) : 0;
+	right_height = tree->right ? 1 + binary_tree_balance_helper(tree->right) : 0;
+	return (left_height > right_height ? left_height : right_height);
 }
