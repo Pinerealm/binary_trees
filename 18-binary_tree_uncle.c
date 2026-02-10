7810045f589ce17f83b2c699ed4851d636566576
Pinerealm <osalami09@gmail.com>
2026-02-10 20:56:55 +0300

    Refactor binary_tree_uncle to remove sibling function and simplify uncle retrieval logic

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-binary_tree_t *_binary_tree_sibling(binary_tree_t *node);
 
 /**
  * binary_tree_uncle - Finds the uncle of a node
@@ -9,28 +8,17 @@ binary_tree_t *_binary_tree_sibling(binary_tree_t *node);
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *parent;
+	binary_tree_t *parent, *grandparent;
 
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
+
 	parent = node->parent;
-	return (_binary_tree_sibling(parent));
-}
-
-/**
- * _binary_tree_sibling - Finds the sibling of a node
- * @node: Pointer to the node to find the sibling
- *
- * Return: Pointer to the sibling node, otherwise NULL
- */
-binary_tree_t *_binary_tree_sibling(binary_tree_t *node)
-{
-	binary_tree_t *parent;
-
-	if (node == NULL || node->parent == NULL)
+	grandparent = parent->parent;
+	if (!grandparent)
 		return (NULL);
-	parent = node->parent;
-	if (parent->left == node)
-		return (parent->right);
-	return (parent->left);
+
+	if (grandparent->left == parent)
+		return (grandparent->right);
+	return (grandparent->left);
 }
