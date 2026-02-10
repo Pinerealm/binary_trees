df75f1856c77de412771abb1a1e07b3a6a3d73a8
Pinerealm <osalami09@gmail.com>
2026-02-10 21:16:27 +0300

    Refactor binary_trees_ancestor to simplify ancestor retrieval logic and improve readability

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,31 +1,62 @@
 #include "binary_trees.h"
 
 /**
+ * node_depth - Measures the depth of a node in a binary tree
+ * @node: Pointer to the node to be measured
+ *
+ * Return: The depth of the node
+ */
+static size_t node_depth(const binary_tree_t *node)
+{
+	size_t depth = 0;
+
+	while (node)
+	{
+		depth++;
+		node = node->parent;
+	}
+	return (depth);
+}
+
+/**
  * binary_trees_ancestor - Finds the lowest common ancestor of two nodes
+ * in a binary tree
  * @first: Pointer to the first node
  * @second: Pointer to the second node
  *
- * Return: Pointer to the lowest common ancestor node of the two given nodes,
- * otherwise NULL
+ * Return: Pointer to the lowest common ancestor node, or NULL if no common
+ * ancestor is found
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 				     const binary_tree_t *second)
 {
-	binary_tree_t *ancestor;
+	size_t depth_first, depth_second;
 
-	if (first == NULL || second == NULL)
+	if (!first || !second)
 		return (NULL);
-	ancestor = (binary_tree_t *)second;
-	while (first != NULL)
+
+	depth_first = node_depth(first);
+	depth_second = node_depth(second);
+
+	while (depth_first > depth_second)
 	{
-		while (second != NULL)
-		{
-			if (first == second)
-				return ((binary_tree_t *)first);
-			second = second->parent;
-		}
-		second = ancestor;
 		first = first->parent;
+		depth_first--;
 	}
+
+	while (depth_second > depth_first)
+	{
+		second = second->parent;
+		depth_second--;
+	}
+
+	while (first && second)
+	{
+		if (first == second)
+			return ((binary_tree_t *)first);
+		first = first->parent;
+		second = second->parent;
+	}
+
 	return (NULL);
 }
