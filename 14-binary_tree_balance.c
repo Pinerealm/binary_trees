a5caa97383b29a620fbfa9dd2621cbf6f502f08b
Pinerealm <osalami09@gmail.com>
2026-02-10 19:32:51 +0300

    Remove binary_tree_balance_helper and simplify balance calculation in binary_tree_balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-int binary_tree_balance_helper(const binary_tree_t *tree);
 
 /**
  * binary_tree_balance - Measures the balance factor of a binary tree
@@ -14,22 +13,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	l_factor = tree->left ? 1 + binary_tree_balance_helper(tree->left) : 0;
-	r_factor = tree->right ? 1 + binary_tree_balance_helper(tree->right) : 0;
+	l_factor = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
+	r_factor = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
 	return (l_factor - r_factor);
-}
-
-/**
- * binary_tree_balance_helper - Helper function for binary_tree_balance
- * @tree: Pointer to the root node of the tree
- *
- * Return: Balance factor of the tree
- */
-int binary_tree_balance_helper(const binary_tree_t *tree)
-{
-	int l_height, r_height;
-
-	l_height = tree->left ? 1 + binary_tree_balance_helper(tree->left) : 0;
-	r_height = tree->right ? 1 + binary_tree_balance_helper(tree->right) : 0;
-	return (l_height > r_height ? l_height : r_height);
 }
