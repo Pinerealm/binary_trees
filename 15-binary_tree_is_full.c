1549e9b80e2fa007d8fdccdb5f21c00af2d21710
Pinerealm <osalami09@gmail.com>
2026-02-10 19:33:13 +0300

    Refactor binary_tree_is_full to simplify logic and improve readability

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -12,10 +12,11 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	left = tree->left ? binary_tree_is_full(tree->left) : 0;
-	right = tree->right ? binary_tree_is_full(tree->right) : 0;
-
-	if (left == right)
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	return (0);
+
+	left = binary_tree_is_full(tree->left);
+	right = binary_tree_is_full(tree->right);
+
+	return (left && right);
 }
