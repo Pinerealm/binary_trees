4cd5115445e8aeabc8cde72780c94e1e547dae94
Pinerealm <osalami09@gmail.com>
2026-02-10 19:32:37 +0300

    Refactor binary_tree_nodes to simplify node counting logic

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -12,13 +12,10 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	if (tree->left == NULL && tree->right == NULL)
-		return (0);
 
-	nodes = binary_tree_nodes(tree->left);
+	nodes = tree->left || tree->right;
+	nodes += binary_tree_nodes(tree->left);
 	nodes += binary_tree_nodes(tree->right);
-	if (tree->left != NULL || tree->right != NULL)
-		return (nodes + 1);
 
 	return (nodes);
 }
