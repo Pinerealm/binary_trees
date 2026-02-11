dae165547fecaa8d53540f92d31802de3f65ffe5
Pinerealm <osalami09@gmail.com>
2026-02-11 09:17:32 +0300

    Refactor binary_tree_balance to improve balance factor calculation and add tree_height function for clarity

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+static size_t tree_height(const binary_tree_t *tree);
 
 /**
  * binary_tree_balance - Measures the balance factor of a binary tree
@@ -8,12 +9,29 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int r_factor, l_factor;
+	size_t r_height, l_height;
 
 	if (tree == NULL)
 		return (0);
 
-	l_factor = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
-	r_factor = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
-	return (l_factor - r_factor);
+	l_height = tree_height(tree->left);
+	r_height = tree_height(tree->right);
+	return ((int)l_height - (int)r_height);
+}
+
+/**
+ * tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree
+ *
+ * Return: Height of the tree
+ */
+static size_t tree_height(const binary_tree_t *tree)
+{
+	size_t height_l, height_r;
+
+	if (tree == NULL)
+		return (0);
+	height_l = tree_height(tree->left);
+	height_r = tree_height(tree->right);
+	return (1 + (height_l > height_r ? height_l : height_r));
 }
