a69421aa1ebe8ca56c295bef5705e242a6422514
Pinerealm <osalami09@gmail.com>
2026-02-11 08:17:11 +0300

    Refactor binary_tree_levelorder to simplify traversal logic and improve readability

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,61 +1,30 @@
 #include "binary_trees.h"
-void print_level(const binary_tree_t *tree, int level, void (*func)(int));
-int _binary_tree_height(const binary_tree_t *tree);
 
 /**
- * binary_tree_levelorder - Traverses a binary tree using level-order traversal
- * @tree: Pointer to the root node of the tree
- * @func: Pointer to a function to call for each node
+ * binary_tree_levelorder - goes through a binary tree using level-order
+ * traversal
+ * @tree: pointer to the root node of the tree to traverse
+ * @func: pointer to a function to call for each node. The value in the
+ * node must be passed as a parameter to this function
+ *
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-	binary_tree_t *node;
-	int height, i;
+	const binary_tree_t *queue[1024], *current;
+	int front = 0, rear = 0;
 
 	if (tree == NULL || func == NULL)
 		return;
-	height = _binary_tree_height(tree);
-	for (i = 0; i <= height; i++)
+
+	queue[rear++] = tree;
+	while (front < rear)
 	{
-		node = (binary_tree_t *)tree;
-		print_level(node, i, func);
+		current = queue[front++];
+
+		func(current->n);
+		if (current->left)
+			queue[rear++] = current->left;
+		if (current->right)
+			queue[rear++] = current->right;
 	}
-}
-
-/**
- * print_level - Prints the nodes at a given level
- * @tree: Pointer to the root node of the tree
- * @level: Level of the tree to print
- * @func: Pointer to a function to call for each node
- */
-void print_level(const binary_tree_t *tree, int level, void (*func)(int))
-{
-	if (tree == NULL)
-		return;
-	if (level == 0)
-		func(tree->n);
-	else if (level > 0)
-	{
-		print_level(tree->left, level - 1, func);
-		print_level(tree->right, level - 1, func);
-	}
-}
-
-/**
- * _binary_tree_height - Measures the height of a binary tree
- * @tree: Pointer to the root node of the tree
- *
- * Return: Height of the tree, or 0 if tree is NULL
- */
-int _binary_tree_height(const binary_tree_t *tree)
-{
-	int left_height, right_height;
-
-	if (tree == NULL)
-		return (0);
-	left_height = _binary_tree_height(tree->left);
-	right_height = _binary_tree_height(tree->right);
-	if (left_height > right_height)
-		return (left_height + 1);
-	return (right_height + 1);
 }
